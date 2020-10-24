bbd474b9df7878106cf0f9fb79f4cdd8db77a3e9
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-10-24 23:33:40 +0300

    24.10 23:33

---
+++ src/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Toolbar.css';
+
+
+class Toolbar extends Component {
+    render() {
+        return (
+            
+            <div className="toolbar">
+                Shafrir
+            </div>
+        );
+    }
+}
+
+export default Toolbar;
---
+++ src/Vipline/Vipline.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Vipline.css';
+
+
+class Vipline extends Component {
+    render() {
+        return (
+            <div className="text">
+                Kashi
+            </div>
+        );
+    }
+}
+
+export default Vipline;
