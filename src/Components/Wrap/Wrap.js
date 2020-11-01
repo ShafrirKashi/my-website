c5ad1e186bcdad987439786df5ee35fd8b67865b
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-01 21:02:32 +0200

    1.11 21:02

--- src/Components/Wrap/Wrap.js
+++ src/Components/Wrap/Wrap.js
@@ -10,6 +10,8 @@ class Wrap extends Component {
                 <pre className="text_3">Coming.</pre>
                 <pre className="text_4">The ultimate cold-weather answer. <br/>
                  Extra soft. Warm to the core.</pre>
+                 <button class="button1">Shop Men</button> 
+                 <button class="button2">Shop Women</button> 
             </div>
         );
     }
