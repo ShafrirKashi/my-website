56cde0e93a0a517ac974972ee38d0f46fef89e25
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-02 20:10:51 +0200

    2.11 20:10

--- src/Components/Container1/Container1.js
+++ src/Components/Container1/Container1.js
@@ -7,7 +7,9 @@ class Container1 extends Component {
             <div>
                 <div className="container1image">
                   <div className="textdiv1">
-                    
+                    <h1 className="h1">Winter Is Coming</h1>
+                    <h2 className="h2">Built for unforgiving conditions. 
+                    <br/>Trusted by athletes across the globe.</h2>
                   </div>
                 </div>
             </div>
