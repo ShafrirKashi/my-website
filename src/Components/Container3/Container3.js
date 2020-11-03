e26cc1111ee1e2c8a5548e06651ffa351ec6e786
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-03 20:38:16 +0200

    3.11 20:38

--- src/Components/Container3/Container3.js
+++ src/Components/Container3/Container3.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Container3.css'
+import Picturebox from '../Picturebox/Picturebox.js'
 
 class Container3 extends Component {
     render() {
         return (
             <div className="container3">
-                asds
+                <Picturebox />
             </div>
         );
     }
--- src/Components/Picturebox/Picturebox.js
+++ src/Components/Picturebox/Picturebox.js
@@ -4,9 +4,24 @@ import './Picturebox.css'
 class Picturebox extends Component {
     render() {
         return (
-            <div className="box">
-                asddsa
+        <div className="container3grid">
+            <div className="box1">
+                <div className="box1text">All-Weather Hiking Gear</div>
+                <a href="22" class="buttonbox1">Shop Now</a>
             </div>
+            <div className="box2">
+                <div className="box2text">Ultimate Breathable Protection</div>
+                <a href="33" class="buttonbox2">Shop Now</a>
+            </div>
+            <div className="box3">
+                <div className="box3text"></div>
+                <a href="44" class="buttonbox3">Shop For Women</a>
+            </div>
+            <div className="box4">
+                <div className="box4text"></div>
+                <a href="55" class="buttonbox4">Shop For Men</a>
+            </div>
+        </div>   
         );
     }
 }
