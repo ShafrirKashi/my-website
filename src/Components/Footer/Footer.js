8dd533894becbebd07547a54f61648f1c99d21ed
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-03 22:40:29 +0200

    3.11 22:40

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,17 @@ class Footer extends Component {
     render() {
         return (
             <div className="foot">
-                Footer here
+                <div className="footop">
+                    <div className="overlay">
+                        <div className="square">
+                            <div className="logo"></div>
+                            <div className="ad">VIPeak</div>
+                        </div>
+                        <div className="textoverlay">Join And Get 10% Off</div>
+                    </div>
+                </div>
+                <div className="footer"></div>
+                <div className="footbottom">© Alpine.com | Created and Designed by Shafrir Kashi</div>
             </div>
         );
     }
