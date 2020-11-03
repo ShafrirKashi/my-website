6a449cf577fc0214bd8d44d806cf8ed2413686fb
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-03 12:59:39 +0200

    3.11 12:59

--- src/Components/Container2/Container2.js
+++ src/Components/Container2/Container2.js
@@ -5,7 +5,7 @@ class FirstWrap extends Component {
     render() {
         return (
             <div className="secondcontainer">
-                <div className="wrap__text1"> asdasd
+                <div className="wrap__text1"> 
                 <h1 className="h1_2">Reset Warmth</h1>
                 <h2 className="h2_2">Unrestricted mobility. Lightweight, packable down. The perfect winter jacket isn’t a stretch—it’s a fact.</h2>
                 <button class="button2">Shop Now</button>  
