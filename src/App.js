29c7e7c294f8999de70b24783609c8854bbd5707
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-05 05:13:46 +0200

    5.11 5:13

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import Container3 from './Components/Container3/Container3'
 // import Container4 from './Components/Container4/Container4'
 import Swiper from './Components/Swiper/Swiper'
 import Container1 from  './Components/Container1/Container1'
+import HeroSlider from './Components/Swiper/HeroSlider'
 
 function App() {
   return (
     <div className="App">  
      <div><Navbar/></div>
+       <div className="container-fluid">
+         <div className="row">
+           <div className="col-md-12 px-0">
+             <HeroSlider/>
+           </div>
+         </div>
+       </div>
       <div><Container1/></div>
       <div><Container2/></div>
       <div><Container3/></div>
---
+++ src/Components/Swiper/HeroSlider.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+
+// React ID Swiper
+import 'swiper/css/swiper.css'
+import Swiper from 'react-id-swiper';
+
+import './HeroSlider.css'
+
+// Images
+import image1 from '../Background/image1.jpg'
+import image2 from '../Background/image2.jpg'
+import image3 from '../Background/image3.jpg'
+
+import { Parallax } from 'swiper';
+
+// Slider Configuation Options 
+
+const  HeroSliderConfigs = {
+containerclass:"swiper-container hero-slider",
+parallax: true,
+centeredSlides:true,
+speed:500,
+spaceBetween:0,
+effect: 'slide'
+};
+
+// Slider Componant
+
+const HeroSlider = () => {
+    const [parallaxSwiper, setParallaxSwiper] = useState(null);
+    const  parallaxAmount = parallaxSwiper ? parallaxSwiper.width * 0.95 : 0;
+    const parallaxOpacity = 0.5;
+
+return( 
+
+    <Swiper {...HeroSliderConfigs} getSwiper={setParallaxSwiper}>
+        <div className="hero-slide">
+
+          <div className="slide-image" data-swiper-parallax={parallaxAmount} 
+               data-swiper-parallax-opacity= {parallaxOpacity}>
+            <img src={image1} alt="image1" className="image"/>
+              <div className="textonslider">
+                <h1 className="text-uppercase mb-2 font-weight-bold"> Slide 1</h1>
+                  <p className="mb-5 small">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Laborum adipisci assumenda nostrum id, quae asperiores ratione accusamus possimus dolorem
+              cumque minus doloremque quis quas modi, placeat eligendi impedit debitis harum?</p>
+        </div>
+         </div>
+      
+    </div>
+    {/* 2 */}
+    <div className="hero-slide">
+
+        <div className="slide-image" data-swiper-parallax={parallaxAmount} 
+        data-swiper-parallax-opacity= {parallaxOpacity}>
+            <img src={image2} alt="image2" className="image"/>
+              <div className="textonslider">
+                <h1 className="text-uppercase mb-2 font-weight-bold"> Slide 1</h1>
+                  <p className="mb-5 small">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Laborum adipisci assumenda nostrum id, quae asperiores ratione accusamus possimus dolorem
+              cumque minus doloremque quis quas modi, placeat eligendi impedit debitis harum?</p>
+        </div>
+    </div>
+       
+    </div>
+    {/* 3 */}
+    <div className="hero-slide">
+
+        <div className="slide-image" data-swiper-parallax={parallaxAmount} 
+        data-swiper-parallax-opacity= {parallaxOpacity}>
+            <img src={image3} alt="image3" className="image"/>
+            <div className="textonslider">
+            <h1 className="text-uppercase mb-2 font-weight-bold"> Slide 1</h1>
+            <p className="mb-5 small">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+             Laborum adipisci assumenda nostrum id, quae asperiores ratione accusamus possimus dolorem
+              cumque minus doloremque quis quas modi, placeat eligendi impedit debitis harum?</p>
+        </div>
+    </div>
+      
+    </div>
+</Swiper>
+)
+};
+
+export default HeroSlider;
