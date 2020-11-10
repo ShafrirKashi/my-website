a44ee0d054c1e3858d061050031c33772a88c7e1
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-10 05:19:36 +0200

    10.11 5:19

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import './App.css';
+
+//Components
+
 import Navbar from './Components/Navbar/Navbar'
+import Container1 from  './Components/Container1/Container1'
 import Container2 from './Components/Container2/Container2'
-import Footer from './Components/Footer/Footer'
 import Container3 from './Components/Container3/Container3'
-// import Container4 from './Components/Container4/Container4'
-<<<<<<< HEAD
 import Swiper2 from './Components/Swiper2/Swiper'
-import Container1 from  './Components/Container1/Container1'
-=======
-// import Swiper2 from './Components/Swiper2/Swiper'
-import Container1 from  './Components/Container1/Container1'
-// import HeroSlider from './Components/Swiper/HeroSlider'
->>>>>>> 8658b75a504131dda7e0ce2ece01e86148544b47
+import Footer from './Components/Footer/Footer'
+
+
 
 function App() {
   return (
     <div className="App">  
      <div><Navbar/></div>
-<<<<<<< HEAD
       <div><Container1/></div>
       <div><Container2/></div>
       <div><Container3/></div>
-      <div><Swiper2 /></div>
-=======
-       {/* <div><HeroSlider /></div> */}
-       {/* <div><Swiper2 /></div> */}
-      <div><Container1/></div>
-      <div><Container2/></div>
-      <div><Container3/></div>
-      {/* <div><Swiper/></div> */}
->>>>>>> 8658b75a504131dda7e0ce2ece01e86148544b47
+      <div><Swiper2/></div> 
       <div><Footer/></div>
     </div>
     
--- src/Components/Swiper2/Swiper.js
+++ src/Components/Swiper2/Swiper.js
@@ -3,18 +3,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import './Swiperbox.css'
 
-// import 'swiper/swiper.scss';
-// import 'swiper/components/navigation/navigation.scss';
-// import 'swiper/components/pagination/pagination.scss';
-// import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Swiperbox () {
     return (
       <div className="Swiperbox" style={{marginTop: "177px"}}>
-        <div className="swiperwrap1"> 
-        <div className="swiperh1">sss</div>
+        {/* <div className="swiperwrap1">  */}
+        {/* <div className="swiperh1">sss</div> */}
         <Swiper
           spaceBetween={-200}
           slidesPerView={3}
@@ -22,33 +22,33 @@ function Swiperbox () {
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
         >
-          <SwiperSlide><div className="Swiper1"></div>
+          <SwiperSlide><div className="Swiper1">1</div>
               sss
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper2"></div>
+          <SwiperSlide><div className="Swiper2">2</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper3"></div>
+          <SwiperSlide><div className="Swiper3">3</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper4"></div>
+          <SwiperSlide><div className="Swiper4">4</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper5"></div>
+          <SwiperSlide><div className="Swiper5">5</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper6"></div>
+          <SwiperSlide><div className="Swiper6">6</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper7"></div>
+          <SwiperSlide><div className="Swiper7">7</div>
           
           </SwiperSlide>
-          <SwiperSlide><div className="Swiper8"></div>
+          <SwiperSlide><div className="Swiper8">8</div>
           
           </SwiperSlide>
         </Swiper>
         </div>
-        </div>
+        // </div>
       );
     
 }
