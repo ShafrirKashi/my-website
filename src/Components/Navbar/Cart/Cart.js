febf1471d946c686a4dc5c51ff2bbabac6cfbd4b
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-11-23 21:00:13 +0200

    23.11 21:00

--- src/Components/Navbar/Cart/Cart.js
+++ src/Components/Navbar/Cart/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Cart.css'
+import Backdrop from './Cartbackdrop/Cartbackdrop'
 
 
 
 const Cart = props => (
-  <p>asdasd</p>
+ <div>
+  <div className="pop">
+    <h2 className="cartheader">Shopping Cart</h2>
+      <div className="productbox">
+        <div className="product"></div>
+      </div>
+    <div className="totalbox">
+      <h3 className="total">Total</h3>
+      <h4 className="totnumber">$124.23</h4>
+    </div>
+    <div className="cartfooter">
+      <div className="checkout">Checkout</div>
+      <div className="viewcart">View cart</div>
+    </div>
+  </div>
+  <Backdrop />
+  </div>
+  
+
+  
 
 
 
