02fb32947589f0ec5bb26718e2aa4baf28e08fc3
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-12-01 14:18:11 +0200

    1.12 14:18

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,24 +15,26 @@ class Footer extends Component {
                     </div>
                 </div>
                 <div className="footer">
-            <div className="footerfirst">dd</div>
+            <div className="footerfirst">
+                <div className="footerlogo"></div>
+            </div>
             <div className="footersecond">
                 <ul className="ss">
                     <h2 >Information</h2>
-                    <li>About Us</li>
-                    <li>More Search</li>
-                    <li>Blog</li>
-                    <li>Testamonials</li>
-                    <li>Events</li>
+                    <li className="about">About Us</li>
+                    <li className="more">More Search</li>
+                    <li className="blog">Blog</li>
+                    <li className="testamonials">Testamonials</li>
+                    <li className="events">Events</li>
                 </ul>
             </div>
             <div className="footerthird">
                 <ul className="ss">
                     <h2>Helpful Links</h2>
-                    <li>Services</li>
-                    <li>Support</li>
-                    <li>Ters & Conditions</li>
-                    <li>Privacy Policy</li>
+                    <li className="services">Services</li>
+                    <li className="support">Support</li>
+                    <li className="terms">Terms & Conditions</li>
+                    <li className="privacy">Privacy Policy</li>
                 </ul>
             </div>
             <div className="footersub">ff</div>
