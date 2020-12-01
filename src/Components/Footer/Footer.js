1565435e9423179b34e6dbae1988f80f3af6dfda
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2020-12-01 02:07:50 +0200

    1.12 2:07

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -16,10 +16,45 @@ class Footer extends Component {
                 </div>
                 <div className="footer">
             <div className="footerfirst">dd</div>
-            <div className="footersecond">ee</div>
-            <div className="footerthird">cc</div>
+            <div className="footersecond">
+                <ul className="ss">
+                    <h2 >Information</h2>
+                    <li>About Us</li>
+                    <li>More Search</li>
+                    <li>Blog</li>
+                    <li>Testamonials</li>
+                    <li>Events</li>
+                </ul>
+            </div>
+            <div className="footerthird">
+                <ul className="ss">
+                    <h2>Helpful Links</h2>
+                    <li>Services</li>
+                    <li>Support</li>
+                    <li>Ters & Conditions</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
             <div className="footersub">ff</div>
-            <div className="footbottom">© Alpine.com | Created and Designed by Shafrir Kashi</div>
+            <div className="footbottom">
+                <div className="footersocial">
+                  
+                <a href="http://facebook.com">
+                    <div className="facebook"></div>
+                </a>
+
+                <a href="http://instagram.com">
+                    <div className="insta"></div>
+                </a>
+                <a href="http://twitter.com">
+                    <div className="twitter"></div>
+                </a>
+                <a href="https://en.wikipedia.org/wiki/Google%2B">
+                    <div className="google"></div>
+                </a>
+                </div>
+                <div className="footercredit">© Alpine.com | Created and Designed by Shafrir Kashi</div>
+            </div>
                 </div>
                
             </div>
