67b81aacf2a03b0543f10c0504103959a0eb9444
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2021-01-17 22:14:51 +0200

    17.1.21 22:14

--- src/Components/Navbar/Login/LoginContent.js
+++ src/Components/Navbar/Login/LoginContent.js
@@ -17,8 +17,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="">
+        Alpine
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
