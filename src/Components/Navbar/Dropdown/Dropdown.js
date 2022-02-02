f4f255202d1db07ba266c7b0d54cafbf26cfbab1
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2022-02-02 20:48:17 +0200

    2.2.22 20:48

--- src/Components/Navbar/Dropdown/Dropdown.js
+++ src/Components/Navbar/Dropdown/Dropdown.js
@@ -7,7 +7,7 @@ const Dropdown = (props) => {
     return (
             <div className="dropdown">
               <div className="links">
-                  <li className="droplink">1</li>
+                  <li className="droplink">11</li>
                   <li className="droplink">2</li>
                   <li className="droplink">3</li>
                   <li className="droplink">4</li>
